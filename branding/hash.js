e2001f9a01b8072731eb9b6bb09ef558f6cf6e65
Matthew Lapora <matthew.lapora@viacommix.com>
2013-03-11 17:22:20 -0400

    Added a negative set of test cases, but commented out.

--- branding/hash.js
+++ branding/hash.js
@@ -15,13 +15,18 @@ var links = [
 
 var brands = ["mtv", "vh1", "cmt", "wrong", ""];
 
-casper.start().each(links, function(self, link) {
+casper.start().each(links, function(self,link) {
     self.each(brands, function(self,brand) {
-    	self.thenOpen(link+"#"+brand, function() {
-        	this.echo(this.getCurrentUrl(), "INFO");
+    	this.then( function() {
+    		this.echo("opening ----"+link+"#"+brand);
+    		this.clear();
     	});
 
-    	self.then(function() {
+    	this.thenOpen(link+"#"+brand, function() {
+        	this.echo(this.getCurrentUrl(), "INFO");
+        });
+        
+    	this.then(function() {
     		if (brand === "mtv") {
     			this.test.assertExists('a#logo.mtv',"Found 'mtv' class on logo element");
 				this.test.assertNotExists('a#logo.vh1',"Did NOT find 'vh1' class on logo element");
@@ -37,16 +42,39 @@ casper.start().each(links, function(self, link) {
 				this.test.assertNotExists('a#logo.mtv',"Did NOT find 'mtv' class on logo element");
 				this.test.assertNotExists('a#logo.vh1',"Did NOT find 'vh1' class on logo element");
     		}
-    		else {
-    			//this.test.assertExists('a#logo.vh1',"Found 'vh1' class on logo element");
-    			// Need to test for case where brand is not available
-    		}
+
+    	// 	else {
+    	// 		// Need to test for case where brand is not available
+    	// 		this.debugHTML('#page_navigation');
+    	// 		var l = this.getCurrentUrl();
+    	// 		var b = l.split('/')[2].split('.')[1]
+    	// 		this.echo("testing ............. L = "+l+" and B ="+ b);
+    			
+    	// 		if (b.indexOf('mtv') > -1)  {
+    	// 			this.echo('MTV domain found');
+	    // 			this.test.assertExists('a#logo.mtv',"Found 'mtv' class on logo element");
+					// this.test.assertNotExists('a#logo.vh1',"Did NOT find 'vh1' class on logo element");
+					// this.test.assertNotExists('a#logo.cmt',"Did NOT find 'cmt' class on logo element");	
+	    // 		}
+	    // 		else if (b.indexOf('vh1') > -1) {
+	    // 			this.echo('VH1 domain found');
+	    // 			this.test.assertExists('a#logo.vh1',"Found 'vh1' class on logo element");
+					// this.test.assertNotExists('a#logo.mtv',"Did NOT find 'mtv' class on logo element");
+					// this.test.assertNotExists('a#logo.cmt',"Did NOT find 'cmt' class on logo element");
+	    // 		}
+	    // 		else if (b.indexOf('cmt') > -1) {
+	    // 			this.echo('CMT domain found');
+	    // 			this.test.assertExists('a#logo.cmt',"Found 'cmt' class on logo element");
+					// this.test.assertNotExists('a#logo.mtv',"Did NOT find 'mtv' class on logo element");
+					// this.test.assertNotExists('a#logo.vh1',"Did NOT find 'vh1' class on logo element");
+	    // 		}
+    	// 	}
 		});
 
 		this.then(function(){
+			// Clears out cookies and cache, to be able to re-create a new branding session
 			this.clear();
 		});
-
     });
 });
 
