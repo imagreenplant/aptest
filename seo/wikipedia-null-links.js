7d1192e5bc881dc96bf8ce0aed1c8cd771572f04
Matthew Lapora <matthew.lapora@viacommix.com>
2013-03-13 12:01:20 -0400

    Added wikipedia null cases update for negative testing. MLAP-1837

--- seo/wikipedia-null-links.js
+++ seo/wikipedia-null-links.js
@@ -1,19 +1,33 @@
 var base_domain = 'http://www.mtv-d.mtvi.com/';
 var test_urls = ['artists/ake-zetterstrom/', 'artists/ace-ventura/'];
+var negative_test_urls = ['artists/lady-gaga/', 'artists/rihanna/', 'artists/band-of-horses/'];
 var links = [];
+var negative_case_links = [];
 
 // Create links for each() command.
 for (var i=0;i<test_urls.length;i++) {
 	links.push(base_domain+test_urls[i]);
 }
 
+// Create negative testing links for each() command.
+for (var j=0;j<negative_test_urls.length;j++) {
+	negative_case_links.push(base_domain+negative_test_urls[j]);
+}
+
 //Some artists had biographies set to null.  We should check for this, and remove the bio.  This test case tests that logic.
 //http://jira.mtvi.com/browse/MLAP-1837
 casper.start().each(links, function(self, link) {
 	self.thenOpen(link, function() {
-        this.echo(this.getTitle());
         this.echo("Testing " + this.getCurrentUrl(), 'INFO');
     	this.test.assertDoesntExist('a#profile_detail_bio', 'The bio element #profile_detail_bio link does not exist.');
+    });
+})
+
+//Just to make sure we didn't break anything, testing the negative case
+casper.each(negative_case_links, function(self, link) {
+	self.thenOpen(link, function() {
+        this.echo("Testing " + this.getCurrentUrl(), 'INFO');
+    	this.test.assertExists('a#profile_detail_bio', 'The bio element #profile_detail_bio link DOES exist.');
     });
 })
 
