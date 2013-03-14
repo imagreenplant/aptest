432f49807d83748c90da131bc357de570e65d003
Matthew Lapora <matthew.lapora@viacommix.com>
2013-03-14 18:15:34 -0400

    Added env support to beta signup test.

--- pages/redirect/beta-sign-up-redirect.js
+++ pages/redirect/beta-sign-up-redirect.js
@@ -1,10 +1,11 @@
-var base_domain = 'http://www.mtv-d.mtvi.com/'
+var base_domain = casper.environment.mtv
 var tested_url = "artist-platform/sign-up/"
 
 //Tests that the artist platform sign-up page has been redirect to AP home.
 //http://jira.mtvi.com/browse/MLAP-1890
 casper.start(base_domain+tested_url, function() {
-    this.test.assertEquals(this.getCurrentUrl(),base_domain+"artists/", "Redirect old beta sign-up page to /artists/ home page.")
+	this.echo(this.getCurrentUrl());
+    this.test.assertEquals(this.getCurrentUrl(),"http://www.mtv.com/artists/", "Redirect old beta sign-up page to /artists/ home page.")
 })
 
 casper.run(function() {
