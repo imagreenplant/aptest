2b690425bd4f1abfc78f0b1a27eaae5175a4700e
Matthew Lapora <matthew.lapora@viacommix.com>
2013-03-14 14:54:12 -0400

    Added Live environment, and modded integration test to use it.

--- integration/CMT/collections.js
+++ integration/CMT/collections.js
@@ -1,21 +1,18 @@
-mtv_q_base_domain = 'http://www.mtv.com'
-
-
 var links = [
-    '/artists/collections/CMT-Crossroads/896718',
-    '/artists/collections/CMT-Edge/896711',
-    '/artists/collections/CMT-Listen-Up/896712',
-    '/artists/collections/CMT-On-Tour/896713',
-    '/artists/collections/CMT-Concrete-Country/896714',
-    '/artists/collections/Live-at-CMT/896715',
-    '/artists/collections/Unplugged/896716'
+    'artists/collections/CMT-Crossroads/896718',
+    'artists/collections/CMT-Edge/896711',
+    'artists/collections/CMT-Listen-Up/896712',
+    'artists/collections/CMT-On-Tour/896713',
+    'artists/collections/CMT-Concrete-Country/896714',
+    'artists/collections/Live-at-CMT/896715',
+    'artists/collections/Unplugged/896716'
 ];
 
 //Tests for cmt domain, that the link was properly redirected.
-var cmtpattern = new RegExp ('^http://www.cmt.com/artists/');
+var cmtpattern = new RegExp ('^'+casper.environment.cmt+'artists/');
 
 casper.start().each(links, function(self, link) {
-    self.thenOpen(mtv_q_base_domain + link, function() {
+    self.thenOpen(this.environment.mtv + link, function() {
         this.echo(this.getTitle());
         // Test to make sure a CMT owned collection, when linked under mtv.com domain, 
         // will redirect to be under a CMT domain.
