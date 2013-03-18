dddf2981117c564fc465feb12f84daf08dbda333
Matthew Lapora <matthew.lapora@viacommix.com>
2013-03-18 18:36:29 -0400

    Modified referrer cases to work by environment

--- branding/cmtreferrer.js
+++ branding/cmtreferrer.js
@@ -1,15 +1,41 @@
-var fakeReferrer = "http://cmt.com";
-var targetURL = 'http://www.mtv-d.mtvi.com/artists/';
- 
+// I have to add the url paramater here to get around the limitation that
+// thenEvaluate() executes within the DOM, and we can't pass variables into it.
+var fakeReferrer = "http://cmt.com?env=" + casper.environment.mtv.slice(7);
 var utils = require('utils');
+
+// casper.on('remote.message', function(message) {
+//     console.log(message);
+// });
 
 casper.start(fakeReferrer, function() {
 	this.echo(this.getCurrentUrl());
 });
  
-casper.thenEvaluate(function(targetURL) {
+// Currently this can only test DEV env.  thenEvaluate is executed within the sandbox of the DOM
+// itself, therefore I can't pass a target href into this function.
+casper.thenEvaluate(function() {
+	//helper function to parse variables from the url 
+	var getUrlVars = function(v) {
+	    var vars = {};
+	    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+	        vars[key] = value;
+	    });
+	    if(v==undefined) {
+	        return vars;
+	    } else {
+	        if(vars[v] !== undefined) {
+	            return vars[v];
+	        } else {
+	            return false;
+	        }
+	    }
+	}
+
+	console.log("This is the new url:" + window.location.href);
+	
+	var newhref = getUrlVars()["env"];
     var link = document.createElement('a');
-	link.setAttribute('href', 'http://www.mtv-d.mtvi.com/artists/');
+    link.setAttribute('href', 'http://' + newhref + 'artists/');
 	link.setAttribute('id', "myTargetUrl");
 	document.body.appendChild(link);
 });
