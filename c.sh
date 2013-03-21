589488fbad6012617bc1a501a8531eac61c12332
Matthew Lapora <matthew.lapora@viacommix.com>
2013-03-21 15:45:15 -0400

    Added starter script

---
+++ c.sh
@@ -0,0 +1,16 @@
+# Starter script for casperjs testing.
+# First variable to enter is the file you want to test, or directory.  
+#
+# Like so:
+# c.sh . 
+# c.sh branding/hash.js
+# c.sh branding/
+
+# The second variable is to choose the environment to test against.
+# Like so:
+# c.sh . live
+# c.sh . dev
+# c.sh . q
+
+echo "Testing $1 on the $2 environment"
+casperjs test $1 --pre=pre.js --$2
