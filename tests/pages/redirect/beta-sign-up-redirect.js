159070f4184a3ed2e9b26a8d6de5e9f65f9149f2
Matthew Lapora <matthew.lapora@viacommix.com>
2013-04-12 13:39:22 -0400

    Removed ability to test dev/q on the beta signup page.

--- tests/pages/redirect/beta-sign-up-redirect.js
+++ tests/pages/redirect/beta-sign-up-redirect.js
@@ -8,6 +8,13 @@ casper.start(base_domain+tested_url, function() {
     this.test.assertEquals(this.getCurrentUrl(),"http://www.mtv.com/artists/", "Redirect old beta sign-up page to /artists/ home page.")
 })
 
-casper.run(function() {
-	this.test.done();
-});
+if (casper.environment.env !== "live") {
+    // This should not be used on Dev/Q as the redirect was not setup on either.
+    casper.echo("This test file is not meant to be used with DEV or Q environments.", "INFO");
+    casper.test.done();
+}
+else {
+    casper.run(function() {
+        this.test.done();
+    });
+}
