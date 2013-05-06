e9d1988a60c9e31cf1408e970299fa213d2d01cd
Matthew Lapora <matthew.lapora@viacommix.com>
2013-05-06 19:42:19 -0400

    Refactor reporting case to use include file for test data shared across other tests.

---
+++ omps.js
@@ -0,0 +1,29 @@
+var omps = {};
+
+omps.ladygaga = {
+    c1:{
+        name: "c1",
+        text: "Lady Gaga",
+        message: "reported c1 is equal to 'Lady Gaga'"
+    },
+    c7:{
+        name: "c7",
+        text: "Unclaimed",
+        message: "reported c7 is set to 'Claimed'"
+    },
+    c14:{
+        name: "c14",
+        text: "Pop",
+        message: "reported c14 is set to 'Pop'"
+    },
+    ch:{
+        name: "ch",
+        text: "artists",
+        message: "reported ch is set to 'artists'"
+    },
+    v49:{
+        name: "v49",
+        text: "artists",
+        message: "reported v49 is set to 'artists'"
+    }
+}
--- tests/reporting/ladygaga-profile.js
+++ tests/reporting/ladygaga-profile.js
@@ -6,34 +6,7 @@ casper.run_reporting_tests = true;
 casper.reporting.actions = {
     "artist-profile:load":      {params:["c1","c7","c14","c28","ch","v49"],c28:"artist page"}
 }
-
-casper.reporting.omps = {
-    c1:{
-        name: "c1",
-        text: "Lady Gaga",
-        message: "reported c1 is equal to 'Lady Gaga'"
-    },
-    c7:{
-        name: "c7",
-        text: "Unclaimed",
-        message: "reported c7 is set to 'Claimed'"
-    },
-    c14:{
-        name: "c14",
-        text: "Pop",
-        message: "reported c14 is set to 'Pop'"
-    },
-    ch:{
-        name: "ch",
-        text: "artists",
-        message: "reported ch is set to 'artists'"
-    },
-    v49:{
-        name: "v49",
-        text: "artists",
-        message: "reported v49 is set to 'artists'"
-    }
-}
+casper.reporting.omps = omps.ladygaga;
 
 casper.test.comment('Testing reporting on Artist Profile page');
 casper.current_event = "artist-profile:load";
