6b9655d76812e7210d33b1126d85ddd1c590d2e5
Matthew Lapora <matthew.lapora@viacommix.com>
2013-05-17 11:10:00 -0400

    Fix case where producers changed text.

--- tests/features/highlights.js
+++ tests/features/highlights.js
@@ -12,7 +12,7 @@ casper.then(function() {
 	this.test.assertExists('table.ap-highlights', "table with class 'ap-highlights' found");
 	// this.test.assertExists('td.copy-container div.copy a'#profile-details-navi', "Header information <ul> exists: bio, hometown...");
 	this.test.assertEquals(this.fetchText('td.copy-container div.copy a'), "Read More", "Should find text 'Read More' for main link");
-	this.test.assertEquals(this.fetchText('td.copy-container div.copy').replace(/(^\s+|\s+$)/g,' '), " CMT Listen Up: JB and the Moonshine Band: Whether they're singing of passionate love or passionate partying, this group brings the vigor to their spirited set. Watch as this Texas group performs \"The Only Drug,\" \"Perfect Girl\" and more during their live performance. Read More ", "should find text");
+	this.test.assertEquals(this.fetchText('td.copy-container div.copy').replace(/(^\s+|\s+$)/g,' '), " CMT Listen Up: Whether they're singing of passionate love or passionate partying, this group brings the vigor to their spirited set. Watch as this Texas group performs \"The Only Drug,\" \"Perfect Girl\" and more during their live performance. Read More ", "should find text");
 	this.test.assertEvalEquals(function() {
         	return document.querySelector('table.ap-highlights td.thumb-container a img').getAttribute('src');
     	}, "http://www.mtv.com/shared/promoimages/cmt/collections/listen_up/140x105.jpg", 'correct CMT Listen up logo url found in img src');
