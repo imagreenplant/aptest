d05273e15fd3251db1808b9b38a4467887e5b47c
Matthew Lapora <matthew.lapora@viacommix.com>
2013-06-10 19:27:13 -0400

    Cleanup profile reporting case

--- tests/reporting/ladygaga-profile.js
+++ tests/reporting/ladygaga-profile.js
@@ -1,8 +1,5 @@
 var url_prefix = casper.environment.mtv;
 
-// This boolean must be set for reporting assertions/tests to occur.
-// casper.run_reporting_tests = true;
-
 casper.reporting.omps = {
     c1:{
         name: "c1",
@@ -31,26 +28,19 @@ casper.reporting.omps = {
     }
 }
 
-
-// casper.turnOnReporting(omps.ladygaga);
-
 // Defines what parameters to check for the selected action.
 casper.reporting.params = {params:["c1","c7","c14","c28","ch","v49"],c28:"artist page"}
-// setupReportingTest(casper, "lady-gaga");
-// casper.reporting.omps = omps.ladygaga;
 
-// casper.test.comment('Testing reporting on Artist Profile page');
-// casper.current_event = "artist-profile:load";
+casper.test.comment('Testing reporting on Lady Gaga artist profile page');
 
 // Viewport needs to be changed AFTER start() is called.
 // Viewport needs to be larger to allow albums and other elements to be loaded in, otherwise
 // the click commands won't work properly.
-
-
 casper.start('').viewport(1000,3000);
 casper.then(function(){
     this.turnOnReporting();
 });
+
 casper.thenOpen(url_prefix + 'artists/lady-gaga/', function() {
     this.wait(2000, function() {
         takePicture(this);  // take picture of artist profile
@@ -58,8 +48,6 @@ casper.thenOpen(url_prefix + 'artists/lady-gaga/', function() {
 });
 
 casper.then(function() {
-    // this.test.comment("Reporting tests complete.", "COMMENT");
-
     // MUST MUST turn off reporting tests or all other tests will have reporting assertions run.
     this.turnOffReporting();
 });
