26ca2773d0d8aa3a0a48bf1756899fd0d1bc659f
Matthew Lapora <matthew.lapora@viacommix.com>
2013-06-10 22:00:41 -0400

    Revise tipjar case for new refactor.

--- tests/reporting/tipjar.js
+++ tests/reporting/tipjar.js
@@ -1,48 +1,38 @@
-// Trigger on requested resources.  Here, the resource is the Omniture gif.
-casper.on('resource.requested', function(resource) {    
-    if ( isOmnitureURL(resource.url) ) {
-        if (this.current_event === "tip_jar:click") {
-            this.test.assert( getParameterByName(resource.url, "pev2") === "Artist Platform Tip Jar Click", "pev2 reported as 'Artist Platform Tip Jar Click'");
-        }
-       
-    }
-});
 
+casper.reporting.omps = {
+    pev2:{
+        name: "pev2",
+        text: "Artist Platform Tip Jar Click",
+        message: "pev2 is reported as 'Artist Platform Tip Jar Click'"
+    }
+}
+
+casper.reporting.params = {params:["pev2"]}
 
 casper.test.comment('Testing tip jar reporting on Artist Profile page');
-// Set the current event to null until we get to the true reporting call.  Otherwise, 
-// other Omniture calls will occur that aren't related to the Tip jar click event.
-casper.current_event = "";
 
-if (casper.environment.env == "live") {
-    var url = casper.environment.mtv + 'artists/lady-gaga/';
-}
-else {
-    var url = casper.environment.mtv + 'artists/the-sawg/';
-}
-
+var url = casper.environment.mtv + 'artists/the-sawg/';
 casper.start(url, function() {
     this.echo(this.getCurrentUrl());
 });
 
-//Preparing for click event by assigning the event type for the parameter parser.
-casper.then(function() {
-	this.current_event = "tip_jar:click";
+casper.then(function(){
+    this.turnOnReporting();
 });
 
 //Click the Tip Jar
 casper.then(function() {
+    this.echo("Clicking 'Leave Tip'", "COMMENT");
     try { 
         this.click("#profile_activity_leave_tip");
     } catch (e) { 
-        this.test.assert(false, "Tip jar button does not exist.");
+        this.test.assert(false, "Tip jar button does not exist on page, but it SHOULD!.");
     } 
 });
 
-//Turn the event back off, so no further reporting is tested.
 casper.then(function() {
-    this.current_event = "";
-    this.on('resource.requested', function() {});
+    // MUST MUST turn off reporting tests or all other tests will have reporting assertions run.
+    this.turnOffReporting();
 });
 
 casper.run(function() {
