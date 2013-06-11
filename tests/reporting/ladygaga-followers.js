47226ec1e2697591fc8db3c615c2c7169c736192
Matthew Lapora <matthew.lapora@viacommix.com>
2013-06-10 20:29:11 -0400

    Add following artists test case.

--- tests/reporting/ladygaga-followers.js
+++ tests/reporting/ladygaga-followers.js
@@ -1,31 +1,54 @@
 var url_prefix = casper.environment.mtv;
 
-// This boolean must be set for reporting assertions/tests to occur.
-casper.run_reporting_tests = true;
+casper.reporting.omps = {
+    c1:{
+        name: "c1",
+        text: "Lady Gaga",
+        message: "reported c1 is equal to 'Lady Gaga'"
+    },
+    c7:{
+        name: "c7",
+        text: ((casper.environment.env === "live") ? "Claimed" :  "Unclaimed"),
+        message: ("reported c7 is set to " + ((casper.environment.env === "live") ? "Claimed" :  "Unclaimed")),
+    },
+    c14:{
+        name: "c14",
+        text: "Pop",
+        message: "reported c14 is set to 'Pop'"
+    },
+    ch:{
+        name: "ch",
+        text: "artists",
+        message: "reported ch is set to 'artists'"
+    },
+    v49:{
+        name: "v49",
+        text: "artists",
+        message: "reported v49 is set to 'artists'"
+    }
+}
 
 // Defines what parameters to check for the selected action.
-casper.reporting.actions = {
-    "follower-artists:load":    {params:["c1","c7","c14","c28","ch","v49"],c28:"related artist page"},
-}
-set_vars_per_env(casper);
-casper.reporting.omps = omps.ladygaga;
+casper.reporting.params = {params:["c1","c7","c14","c28","ch","v49"],c28:"related artist page"}
+
+casper.test.comment('Testing reporting on Lady Gaga following artists page');
 
 // Viewport needs to be changed AFTER start() is called.
 // Viewport needs to be larger to allow albums and other elements to be loaded in, otherwise
 // the click commands won't work properly.
 casper.start('').viewport(1000,3000);
+casper.then(function(){
+    this.turnOnReporting();
+    this.test.comment("Opening following artists page", "COMMENT")
+});
 
-casper.thenOpen(url_prefix + 'artists/lady-gaga/', function() {
-    this.wait(2000, function() {
-        takePicture(this);  // take picture of artist profile
-    });
+casper.thenOpen(url_prefix + 'artists/lady-gaga/related-artists/?filter=followers', function() {
+    this.echo(this.getCurrentUrl() + " loaded", "INFO"); 
 });
 
 casper.then(function() {
-    this.test.comment("Reporting tests complete.", "COMMENT");
-
     // MUST MUST turn off reporting tests or all other tests will have reporting assertions run.
-    this.run_reporting_tests = false;
+    this.turnOffReporting();
 });
 
 casper.run(function() {
