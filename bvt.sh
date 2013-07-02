a51902380189c61f04a7a80cf4b43463653542a7
Matthew Lapora <matthew.lapora@viacommix.com>
2013-07-01 22:53:14 -0400

    Create start script for BVT suite

--- bvt.sh
+++ bvt.sh
@@ -1,1 +1,1 @@
-timeout3 -t 60 -i 1 -d 30 casperjs test tests/ --pre=pre.js,pre2.js --includes=inc.js,omps.js --d
+timeout3 -t 600 -i 5 -d 30 casperjs test bvt/ --pre=pre.js,pre2.js --includes=inc.js,omps.js --d
