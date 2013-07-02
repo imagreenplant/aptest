5e094e98df00ce18567654626c155e638340e858
Matthew Lapora <github@thelaporas.com>
2013-07-02 15:53:30 -0400

    Fix absolute path to timeout script.

--- bvt.sh
+++ bvt.sh
@@ -1,5 +1,5 @@
 export PATH=/usr/local/bin:$PATH
 echo "Testing TRUNK on the DEV environment @ `date`"
 echo "Startin casperjs smoke test of TRUNK on DEV environment @ `date`" >> log.txt
-timeout3 -t 600 -i 5 -d 30 casperjs test /Users/hans/git/aptest/bvt/ --pre=/Users/hans/git/aptest/pre.js,/Users/hans/git/aptest/pre2.js --includes=/Users/hans/git/aptest/inc.js,/Users/hans/git/aptest/omps.js --d
+/Users/hans/git/aptest timeout3 -t 600 -i 5 -d 30 casperjs test /Users/hans/git/aptest/bvt/ --pre=/Users/hans/git/aptest/pre.js,/Users/hans/git/aptest/pre2.js --includes=/Users/hans/git/aptest/inc.js,/Users/hans/git/aptest/omps.js --d
 echo "Finished DEV environment smoke test @ `date`"
