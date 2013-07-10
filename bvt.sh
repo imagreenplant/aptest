60449a7165db43f195365c5c4bc4ca0ea03e38e6
Matthew Lapora <matthew.lapora@viacommix.com>
2013-07-10 14:13:50 -0400

    Remove console colors for jenkins input.

--- bvt.sh
+++ bvt.sh
@@ -1,1 +1,1 @@
-timeout3 -t 600 -i 5 -d 30 casperjs test bvt/ --pre=pre.js,pre2.js --includes=inc.js,omps.js --d
+timeout3 -t 600 -i 5 -d 30 casperjs test bvt/ --no-colors --pre=pre.js,pre2.js --includes=inc.js,omps.js --d
