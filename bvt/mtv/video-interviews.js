8987a029f9469817421ddf73ef8c1666ef6008e8
Matthew Lapora <matthew.lapora@viacommix.com>
2013-07-15 17:14:46 -0400

    Fix error in video interviews text

--- bvt/mtv/video-interviews.js
+++ bvt/mtv/video-interviews.js
@@ -1,8 +1,8 @@
 casper.test.comment('BVT: interviews page.');
 
 casper.start(casper.environment.mtv + 'artists/taylor-swift/video-interviews/', function() {
-    this.test.assertTitle("Lady Gaga | New Music And Songs | MTV", 
-        "Title is equal to Lady Gaga | New Music And Songs | MTV");
+    this.test.assertTitle("Taylor Swift | New Music And Songs | MTV", 
+        "Title is equal to Taylor Swift | New Music And Songs | MTV");
 });
 
 casper.run(function() {
