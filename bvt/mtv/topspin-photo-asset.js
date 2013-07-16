c598ab59a0cd8a696dac53f3010ba140220071d0
Matthew Lapora <matthew.lapora@viacommix.com>
2013-07-15 20:26:51 -0400

    Add topspin photo asset page

---
+++ bvt/mtv/topspin-photo-asset.js
@@ -0,0 +1,20 @@
+var test_data = { 
+	name:"The SAWG", 
+	alias:"the-sawg", 
+	brand:"mtv", 
+	photo_name:"2704 × 2058, 1.2MB",
+	caption:"2704 × 2058, 1.2MB",
+	mgid:"topspin:202"
+}
+
+casper.test.comment('BVT: MTV soundstream page.');
+
+casper.start(casper.environment.mtv + 'artists/the-sawg/photos/topspin:202', function() {
+    this.echo("Opening " + this.getCurrentUrl(), 'INFO');
+    this.test.assertHttpStatus(200, 'Received 200 Status response');
+    testSEOTagsForArtist(this, test_data , "topspin_photo_asset");
+});
+
+casper.run(function() {
+    this.test.done(5);
+});
