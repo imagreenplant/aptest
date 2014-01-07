6d1fdf878728bce41f5c8a1fe7a078e188e4ac2a
Matthew Lapora <mlapora@Matthews-MacBook-Pro.local>
2014-01-07 15:09:58 -0800

    Use LogoTV instead of LOGO for metadata

--- tests/bvt/logo/home.js
+++ tests/bvt/logo/home.js
@@ -2,24 +2,24 @@ casper.test.comment('BVT: Logo home page.');
 
 casper.start(casper.environment.logo + 'artists/', function() {
 	this.echo("Loaded " + this.getCurrentUrl(), "COMMENT");
-    this.test.assertTitle("Artists.Logo | Discover Music Artists and Bands | LOGO", 
-        "Title is equal to Artists.Logo | Discover Music Artists and Bands | LOGO");
+    this.test.assertTitle("LogoTV Artists | Discover Music Artists and Bands | LogoTV", 
+        "Title is equal to 'LogoTV Artists | Discover Music Artists and Bands | LogoTV'");
     this.test.assertEvalEquals(function() {
             return __utils__.findOne('meta[property="og:title"]').content;
-			},	'Artists.Logo | Discover Music Artists and Bands',
-			'og:title is equal to "Artists.Logo | Discover Music Artists and Bands"'
+			},	'LogoTV Artists | Discover Music Artists and Bands',
+			'og:title is equal to "LogoTV Artists | Discover Music Artists and Bands"'
 	);
 	this.test.assertEvalEquals(function() {
             return __utils__.findOne('meta[name="description"]').content;
-        	},	'Find the largest selection of popular artists and best bands on Artists.Logo. Plus, get ' + 
-        		'the latest official updates from Facebook, Twitter, and listen to free music.',
-        	'meta description is equal to "Find the largest selection of popular artists and best bands on Artists.Logo..."'
+        	},	'Find the largest selection of popular artists and best bands on LogoTV' +
+        	' Artists. Plus, get the latest official updates from Facebook, Twitter, and listen to free music.',
+        	'meta description is equal to "Find the largest selection of popular artists and best bands on LogoTV..."'
 	);
 	this.test.assertEvalEquals(function() {
             return __utils__.findOne('meta[property="og:description"]').content;
-			},  'Find the largest selection of popular artists and best bands on Artists.Logo. Plus, get the latest official ' + 
-				'updates from Facebook, Twitter, and listen to free music.',
-			'og:description is equal to "Find the largest selection of popular artists and best bands on Artists.Logo..."'
+			},  'Find the largest selection of popular artists and best bands on LogoTV Artists. ' + 
+			'Plus, get the latest official updates from Facebook, Twitter, and listen to free music.',
+			'og:description is equal to "Find the largest selection of popular artists and best bands on LogoTV	..."'
 	);
 
 
